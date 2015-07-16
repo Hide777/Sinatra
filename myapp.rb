9ad90bbae0f7f76121af57cd3bde8ac913445657
Hide777 <yanagi0806@yahoo.co.jp>
2015-07-16 17:16:26 +0900

    20150716

--- myapp.rb
+++ myapp.rb
@@ -1,19 +1,8 @@
 #encoding: utf-8
-require 'rubygems'
-require 'bundler'
-Bundler.require
-#require 'sinatra'
-require 'sinatra/reloader' if development?
-#require 'sinatra/contrib/all'
-#require 'haml'
-#require 'csv'
-#require 'json'
-require 'open-uri'
-require 'nokogiri'
-require 'robotex'
-
 module App
-  class Application < Sinatra::Base  
+  class Application < Sinatra::Base 
+    class Gauge < ActiveRecord::Base
+    end
     before do
       @edition = "20150714Ed"
     end
@@ -24,18 +13,13 @@ module App
       register Sinatra::ActiveRecordExtension
       set :database, {adapter: "sqlite3", database: "./db/development.sqlite3"}
     end
-   
-    class Gauge < ActiveRecord::Base
-    end  
-   
     def file_convert
       datatime=""
       CSV.foreach("./temp.csv","r") do |data|
          datatime = data
       end
       return datatime
-    end
-        
+    end      
     # routing
     get '/' do
       @message = "Bootstrap3 "
@@ -54,7 +38,17 @@ module App
       haml :'sample/table'
     end
     get '/haml/test' do
-      @message = "Test Routing"
+      url = 'http://www.msn.com/ja-jp'      
+      robotex = Robotex.new
+      @message = robotex.allowed?(url)
+      # user_agentの偽装
+      user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'
+      charset = nil
+      html = open(url, "User-Agent" => user_agent) do |f|
+        charset = f.charset # 文字種別を取得
+        f.read
+      end
+      @doc = Nokogiri::HTML(html, nil, charset)
       haml :'sample/test'
     end
     get '/haml/sc' do
